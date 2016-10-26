56432a32fe3b0c2829321cd4ac370b7643ef5e76
Swarna Kishore <swarnakishoree@gmail.com>
2016-10-25 23:24:44 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,183 @@
+
+angular.module('BlankApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache', 'users'])
+  
+.run(function($log){
+    $log.debug('MyApp is ready now!')
+  })
+.config(function($mdThemingProvider, $mdIconProvider){
+  $mdThemingProvider
+    .theme('default')
+    .primaryPalette('brown')
+    .accentPalette('red');
+  /*https://material.angularjs.org/latest/api/service/$mdIconProvider*/
+  /*id, url, viewBoxSize*/
+  /*viewBoxSize is by default 24, sets width/height of the icons viewBox*/
+  
+  
+$mdIconProvider.icon('share', 'svg/share.svg', 24)
+               .icon('menu' , 'svg/menu.svg', 24);
+     
+});
+
+(function(){
+  'use strict';
+
+  // Prepare the 'users' module for subsequent registration of controllers and delegates
+  angular.module('users', [ 'ngMaterial' ]);
+
+
+})();
+
+(function(){
+  'use strict';
+
+  angular.module('users')
+         .service('userService', ['$q', UserService]);
+
+  /**
+   * Users DataService
+   * Uses embedded, hard-coded data model; acts asynchronously to simulate
+   * remote data service call(s).
+   *
+   * @returns {{loadAll: Function}}
+   * @constructor
+   */
+  function UserService($q){
+    var users = [
+      {
+        name: 'Lia Lugo',
+        avatar: 'svg-1',
+        content: 'I love cheese, especially airedale queso. Cheese and biscuits halloumi cauliflower cheese cottage cheese swiss boursin fondue caerphilly. Cow port-salut camembert de normandie macaroni cheese feta who moved my cheese babybel boursin. Red leicester roquefort boursin squirty cheese jarlsberg blue castello caerphilly chalk and cheese. Lancashire.'
+      },
+      {
+        name: 'George Duke',
+        avatar: 'svg-2',
+        content: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.'
+      },
+      {
+        name: 'Gener Delosreyes',
+        avatar: 'svg-3',
+        content: "Raw denim pour-over readymade Etsy Pitchfork. Four dollar toast pickled locavore bitters McSweeney's blog. Try-hard art party Shoreditch selfies. Odd Future butcher VHS, disrupt pop-up Thundercats chillwave vinyl jean shorts taxidermy master cleanse letterpress Wes Anderson mustache Helvetica. Schlitz bicycle rights chillwave irony lumberhungry Kickstarter next level sriracha typewriter Intelligentsia, migas kogi heirloom tousled. Disrupt 3 wolf moon lomo four loko. Pug mlkshk fanny pack literally hoodie bespoke, put a bird on it Marfa messenger bag kogi VHS."
+      },
+      {
+        name: 'Lawrence Ray',
+        avatar: 'svg-4',
+        content: 'Scratch the furniture spit up on light gray carpet instead of adjacent linoleum so eat a plant, kill a hand pelt around the house and up and down stairs chasing phantoms run in circles, or claw drapes. Always hungry pelt around the house and up and down stairs chasing phantoms.'
+      },
+      {
+        name: 'Ernesto Urbina',
+        avatar: 'svg-5',
+        content: 'Webtwo ipsum dolor sit amet, eskobo chumby doostang bebo. Bubbli greplin stypi prezi mzinga heroku wakoopa, shopify airbnb dogster dopplr gooru jumo, reddit plickers edmodo stypi zillow etsy.'
+      },
+      {
+        name: 'Gani Ferrer',
+        avatar: 'svg-6',
+        content: "Lebowski ipsum yeah? What do you think happens when you get rad? You turn in your library card? Get a new driver's license? Stop being awesome? Dolor sit amet, consectetur adipiscing elit praesent ac magna justo pellentesque ac lectus. You don't go out and make a living dressed like that in the middle of a weekday. Quis elit blandit fringilla a ut turpis praesent felis ligula, malesuada suscipit malesuada."
+      }
+    ];
+
+    // Promise-based API
+    return {
+      loadAllUsers : function() {
+        // Simulate async nature of real remote calls
+        return $q.when(users);
+      }
+    };
+  }
+
+})();
+
+(function(){
+
+  angular
+       .module('users')
+       .controller('UserController', [
+          'userService', '$mdSidenav', '$mdBottomSheet', '$timeout', '$log',
+          UserController
+       ]);
+
+  /**
+   * Main Controller for the Angular Material Starter App
+   * @param $scope
+   * @param $mdSidenav
+   * @param avatarsService
+   * @constructor
+   */
+  function UserController( userService, $mdSidenav, $mdBottomSheet, $timeout, $log ) {
+    var self = this;
+
+    self.selected     = null;
+    self.users        = [ ];
+    self.selectUser   = selectUser;
+    self.toggleList   = toggleUsersList;
+    self.title = 'Angular Material - Starter App';
+    self.share        =share;
+      //  $scope.title = 'UI Components with Dynamic Data';
+
+    // Load all registered users
+
+    userService
+          .loadAllUsers()
+          .then( function( users ) {
+            self.users    = [].concat(users);
+            self.selected = users[0];
+          });
+
+    // *********************************
+    // Internal methods
+    // *********************************
+    /**
+     * Hide or Show the 'left' sideNav area
+     */
+    function toggleUsersList() {
+        $mdSidenav('left').toggle();
+    }
+
+    /**
+     * Select the current avatars
+     * @param menuId
+     */
+    function selectUser ( user ) {
+      self.selected = angular.isNumber(user) ?         $scope.users[user] : user;
+    }
+    
+    /**
+     * Show the bottom sheet
+     *
+     */
+    function share(selectedUser){
+      $mdBottomSheet.show({
+        controller: ContactSheetController,
+        controllerAs: 'vm',
+        templateUrl: 'contactSheet.html',
+        //if you want overlay to be over entire app, you can enter body
+        // parent: angular.element(document.querySelector('#body'))
+        parent: angular.element(document.querySelector('#content'))
+      }).then(function(clickedItem) {
+          $log.debug( clickedItem.name + ' clicked!');
+      });
+
+      
+       /**
+         * User ContactSheet controller
+         */
+        function ContactSheetController( $mdBottomSheet ) {
+          this.user = selectedUser;
+          this.items = [
+            
+            { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'svg/twitter.svg'},
+            { name: 'Google+'     , icon: 'google_plus' , icon_url: 'svg/googlePlus.svg'}
+              ];
+          this.contactUser = function(action) {
+            
+            $mdBottomSheet.hide(action);
+          };
+        }
+      
+    }
+
+
+  
+  }
+
+})();
